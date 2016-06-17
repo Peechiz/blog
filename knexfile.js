6c453961524c3f292af8fb0a666166ad116bb674
Chris Impicciche <peechiz@bakudan.local>
2016-06-17 11:31:32 -0500

    dihn method

--- knexfile.js
+++ knexfile.js
@@ -12,16 +12,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: DATABASE_URL,
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL
   }
 
 };
